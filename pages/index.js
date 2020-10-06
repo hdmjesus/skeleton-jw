56773b87b400275f5a4ef15ea9f9b9bc47f3424e
Wonder Diaz <wonderdxk@gmail.com>
2020-10-05 21:11:44 -0400

    Initial Commit - 0.0.1
    Install Sass, Next, React, React-dom, Webpack, Loader sass and css
    Structure of project
    Hello World in Next.js with page index,js and Style

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import sass from "../styles/style.scss"
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className={sass.title}>I am not red!</h1>
+    </div>
+  )
+}
+
+export default HomePage
